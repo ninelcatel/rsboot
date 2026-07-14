b97ef477472e2a4b71211a34469b97615f01d4c6
ninelcatel <alexnegrisan0@gmail.com>
2026-07-15 02:39:24 +0300

    TUI is mostly done, TODO: make a wrapper for the draw module and add fallbacks for .unwraps()

--- efi/src/draw.rs
+++ efi/src/draw.rs
@@ -4,30 +4,60 @@ use uefi::system;
 
 use crate::environment;
 
-const BACKGROUND: Color = Color::Black;
+const RECT: Color = Color::LightGray;
+const TITLE: &str = "Welcome to";
+const SUBTITLE: &str = "Please select the OS you wish to install.";
+
+pub fn clear_screen() {
+    system::with_stdout(|out| {
+        out.set_color(Color::LightGray, Color::Black).unwrap(); // set colors to default
+        out.clear().unwrap();
+    });
+}
 
 pub fn draw_menu(selected: usize, items: &[&str], env: &environment::ENV) {
     system::with_stdout(|out| {
-        out.clear().unwrap();
         draw_rect(out);
+
+        draw_title(out);
+
         match env {
             environment::ENV::MENU => {
-                for (i, item) in items.iter().enumerate() {
-                    if i == selected {
-                        out.set_color(Color::Magenta, BACKGROUND).unwrap();
-                    } else {
-                        out.set_color(Color::LightGray, BACKGROUND).unwrap();
-                    };
-
-                    center_line(out, item, i);
-
-                    out.write_fmt(format_args!("{item}\n")).unwrap();
+                for i in 0..items.len() {
+                    draw_item(out, items, i, selected);
                 }
             }
             environment::ENV::OS => {
-                uefi::println!("  {}", items[selected]);
+                out.set_color(Color::Magenta, RECT).unwrap();
+
+                center_line(out, items[selected], 0);
+
+                out.write_fmt(format_args!("{}", items[selected])).unwrap();
             }
         }
+    });
+}
+
+fn draw_item(
+    out: &mut uefi::proto::console::text::Output,
+    items: &[&str],
+    i: usize,
+    selected: usize,
+) {
+    if i == selected {
+        out.set_color(Color::Magenta, RECT).unwrap();
+    } else {
+        out.set_color(Color::Black, RECT).unwrap();
+    }
+    center_line(out, items[i], i);
+    out.write_fmt(format_args!("{}", items[i])).unwrap();
+}
+
+// helper function to overwrite only the 2 affected selections
+pub fn update_selection(items: &[&str], old: usize, new: usize) {
+    system::with_stdout(|out| {
+        draw_item(out, items, old, new);
+        draw_item(out, items, new, new);
     });
 }
 
@@ -40,6 +70,31 @@ pub fn get_dimensions(out: &uefi::proto::console::text::Output) -> (usize, usize
         .unwrap();
     (cols, rows)
 }
+
+pub fn draw_title(out: &mut uefi::proto::console::text::Output) {
+    let (cols, rows) = get_dimensions(out);
+    let col = cols.saturating_sub(TITLE.chars().count() + 8) / 2; // + 8 de la " rsboot!"
+    out.set_color(Color::Red, RECT).unwrap();
+
+    out.set_cursor_position(col, rows / 5).unwrap();
+    out.write_fmt(format_args!("{TITLE}")).unwrap();
+
+    out.set_cursor_position(col + TITLE.len() + 1, rows / 5)
+        .unwrap();
+    out.set_color(Color::Yellow, RECT).unwrap();
+    out.write_fmt(format_args!("rsboot")).unwrap();
+
+    out.set_cursor_position(col + TITLE.len() + 7, rows / 5)
+        .unwrap();
+    out.set_color(Color::Red, RECT).unwrap();
+    out.write_fmt(format_args!("!")).unwrap();
+
+    let col2 = cols.saturating_sub(SUBTITLE.chars().count()) / 2;
+
+    out.set_cursor_position(col2, rows / 5 + 1).unwrap();
+    out.write_fmt(format_args!("{SUBTITLE}")).unwrap();
+}
+
 pub fn center_line(out: &mut uefi::proto::console::text::Output, text: &str, row_index: usize) {
     let (cols, rows) = get_dimensions(out);
     let col = cols.saturating_sub(text.chars().count()) / 2;
@@ -49,7 +104,7 @@ pub fn center_line(out: &mut uefi::proto::console::text::Output, text: &str, row
 
 pub fn draw_rect(out: &mut uefi::proto::console::text::Output) {
     let (cols, rows) = get_dimensions(out);
-    out.set_color(Color::Black, Color::LightGray).unwrap();
+    out.set_color(Color::Black, RECT).unwrap();
     let col_start = cols / 5;
     let col_end = cols - col_start;
     let width = col_end - col_start;
--- efi/src/main.rs
+++ efi/src/main.rs
@@ -22,6 +22,7 @@ fn main() -> Status {
     let mut env = ENV::MENU;
     let mut current_pick: usize = 0;
 
+    draw::clear_screen();
     draw::draw_menu(current_pick, &OS, &env);
 
     while running {
@@ -31,14 +32,25 @@ fn main() -> Status {
 
         match (&env, key) {
             (ENV::MENU, Key::Special(ScanCode::UP)) => {
+                let old = current_pick;
                 current_pick = current_pick.saturating_sub(1);
+                if current_pick != old {
+                    draw::update_selection(&OS, old, current_pick);
+                }
+                continue;
             }
             (ENV::MENU, Key::Special(ScanCode::DOWN)) => {
+                let old = current_pick;
                 if current_pick + 1 < OS.len() {
                     current_pick += 1;
+                    draw::update_selection(&OS, old, current_pick);
                 }
+                continue;
             }
-            (ENV::MENU, Key::Special(ScanCode::ESCAPE)) => running = false,
+            (ENV::MENU, Key::Special(ScanCode::ESCAPE)) => {
+                running = false;
+                draw::clear_screen();
+            }
             (ENV::MENU, Key::Printable(c)) if c == Char16::try_from('\r').unwrap() => {
                 env = ENV::OS;
             }
