5ab8c817dac677d5629f961dd5181ac4eed57342
ninelcatel <alexnegrisan0@gmail.com>
2026-07-15 00:54:45 +0300

    centered menu entries

--- efi/src/draw.rs
+++ efi/src/draw.rs
@@ -17,6 +17,9 @@ pub fn draw_menu(selected: usize, items: &[&str], env: &environment::ENV) {
                     } else {
                         out.set_color(Color::LightGray, BACKGROUND).unwrap();
                     };
+
+                    center_line(out, item, i);
+
                     out.write_fmt(format_args!("{item}\n")).unwrap();
                 }
             }
@@ -25,4 +28,16 @@ pub fn draw_menu(selected: usize, items: &[&str], env: &environment::ENV) {
             }
         }
     });
+}
+
+pub fn center_line(out: &mut uefi::proto::console::text::Output, text: &str, row_index: usize) {
+    let (cols, rows) = out
+        .current_mode()
+        .ok()
+        .flatten()
+        .map(|m| (m.columns(), m.rows()))
+        .unwrap();
+    let col = cols.saturating_sub(text.chars().count()) / 2;
+    let row = rows / 3;
+    out.set_cursor_position(col, row + row_index).unwrap();
 }
