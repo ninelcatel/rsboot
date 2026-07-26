dda9df471c7ccaf385c56775c2fa1788c43dc783
ninelcatel <alexnegrisan0@gmail.com>
2026-07-26 08:53:11 +0300

    created the C-like structs and GUIDs , need to locate the protocol and use it

--- efi/src/loadbootable.rs
+++ efi/src/loadbootable.rs
@@ -1,6 +1,26 @@
 // load a .efi into memory and then run it, keeps write/text format as before
 // qemu was set up with the root file system at /esp , a valid path for example would be
 // \\efi\\boot\\hello.efi
+#[repr(C)]
+struct EFI_DEVICE_PATH {
+    r#type: u8,
+    SubType: u8,
+    Length: [u8; 2],
+}
+#[repr(C)]
+struct EFI_RAM_DISK_PROTOCOL {
+    register: unsafe extern "efiapi" fn(
+        RamDiskBase: u64,
+        RamDiskSize: u64,
+        RamDiskType: *const uefi::Guid,
+        ParentDevicePath: *const EFI_DEVICE_PATH,
+        device_path: *mut *const EFI_DEVICE_PATH,
+    ) -> uefi::Status,
+    unregister: unsafe extern "efiapi" fn(DevicePath: *const EFI_DEVICE_PATH) -> uefi::Status,
+}
+
+const RAM_DISK_GUID: uefi::Guid = uefi::guid!("ab38a0df-6873-44a9-87e6-d4eb56148449");
+const VIRTUAL_CD_GUID: uefi::Guid = uefi::guid!("3d5abd30-4175-87ce-6d64-d2ade523c4bb");
 pub fn load_bootable(path: &uefi::CStr16) -> uefi::Result {
     let handler = uefi::boot::image_handle();
 
@@ -20,6 +40,9 @@ pub fn load_bootable(path: &uefi::CStr16) -> uefi::Result {
             file_path: None,
         },
     )?;
+    let protocol =
+        uefi::boot::locate_handle_buffer(uefi::boot::SearchType::ByProtocol(&RAM_DISK_GUID))?;
+
     // allocates memory and loads the buffer in RAM . file path is optional
 
     uefi::boot::start_image(instance)?;
